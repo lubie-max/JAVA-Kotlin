89ca3e1c4b7937faac639e48dc67df2096733c25
lubie-max <shaikhls2421@gmail.com>
2024-07-16 23:55:58 +0530

    inheritance

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Intro i = new Intro("Lubna");
         System.out.println(i);
+        String n = i.name;
+        System.out.println(n);
     }
 
 }
