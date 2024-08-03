84ca3d29a14ee6945699265ea48964661ae0e542
lubie-max <shaikhls2421@gmail.com>
2024-08-03 23:40:25 +0530

    eh

--- src/Exception_Handling/Main.java
+++ src/Exception_Handling/Main.java
@@ -1,5 +1,7 @@
 package Exception_Handling;
 
+import java.security.spec.ECField;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +11,14 @@ public class Main {
         Exception1 f = new Exception1(4);
 
         e.getSquere();
+
+        try{
+           if (e.num != 0){
+               System.out.println("this is an inapproprite error.");
+           }
+        }
+        catch (Exception ae){
+            System.out.println(ae.getCause());
+        }
     }
 }
