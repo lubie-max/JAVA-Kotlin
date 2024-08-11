df589f4cd7ccd8e724e90eb6c34a98897cbea604
lubie-max <shaikhls2421@gmail.com>
2024-08-11 23:56:29 +0530

    strings

---
+++ src/Basics/Strings/Main.java
@@ -0,0 +1,13 @@
+package Basics.Strings;
+
+public class Main {
+
+    public  static  void  main(String[] a){
+
+        String name = "Lubna";
+//        or
+        String dob = "122232";
+
+        System.out.println(name.concat(" " +dob));
+    }
+}
