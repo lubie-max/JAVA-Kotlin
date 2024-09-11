471becafb57a594668e066ba85b618c2a333ff1d
lubie-max <shaikhls2421@gmail.com>
2024-09-12 00:05:41 +0530

    methods

--- src/MultiThreading/Methods.java
+++ src/MultiThreading/Methods.java
@@ -6,16 +6,26 @@ public class Methods extends  Thread{
     public void run(){
 
 
-        for (int i = 0; i <  10 ; i++) {
+        for (int i = 0; i <  5 ; i++) {
+
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
             System.out.println("thread's Priority -> " + Thread.currentThread().getPriority());
             
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Methods t1 = new Methods();
         t1.start();
+        t1.setPriority(1);
+        t1.join();
+
+        System.out.println("Main Thread Execution");
 
     }
 }
