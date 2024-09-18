53f76200060600433b057359b0624bbe34fff7be
lubie-max <shaikhls2421@gmail.com>
2024-09-18 23:50:35 +0530

    Synchronization

---
+++ src/MultiThreading/Synchronization/Counter.java
@@ -0,0 +1,16 @@
+package MultiThreading.Synchronization;
+
+public class Counter {
+
+    private  int counter =0 ;
+
+    public void increament(){
+        counter ++ ;
+    }
+
+    public  int getCounter(){
+        return  this.counter;
+    }
+
+
+}
---
+++ src/MultiThreading/Synchronization/MyThread.java
@@ -0,0 +1,16 @@
+package MultiThreading.Synchronization;
+
+public class MyThread extends  Thread {
+    Counter counter ;
+
+    MyThread(Counter counter){
+        this.counter = counter ;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            counter.increament();
+        }
+    }
+}
